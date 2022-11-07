63d870dd50338a236d1f74100d0a16109bac9e72
David Figone <david.louis.figone@gmail.com>
2022-11-07 11:24:39 -0600

    Initial Commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>()
+            {"Knights of the Old Republic", "Mass Effect", "Fable", "Splinter Cell", "Risk of Rain"};
+
+            IEnumerable<string> gameOrder =
+            videoGames.OrderBy(str => str.Length)
+                           .ThenBy(str => str);
+
+            foreach(string game in gameOrder)
+            Console.WriteLine(game);
+        
+        
+        
+        
+            Console.ReadLine();
         }
     }
 }
